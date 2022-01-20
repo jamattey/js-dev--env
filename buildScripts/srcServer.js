e28483b2423c87e5fa7d5630855c401226668ef5
James Amattey <me@jamattey.com>
2022-01-20 13:19:23 +0000

    Update srcServer.js
    added a rule based override for eslint exception

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -4,7 +4,7 @@ import open from 'open';
 import webpack from 'webpack';
 import config from '../webpack.config.dev';
 
-
+/* eslint-disable no-console */
 const port = 3100;
 const app = express();
 const compiler = webpack(config); //creates a reference to webpack compiler
