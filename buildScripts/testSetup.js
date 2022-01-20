0fe4c9ed6c19c749cf524f79e5a1b35b1967d1a7
James Amattey <me@jamattey.com>
2022-01-20 21:04:18 +0000

    Update testSetup.js
    test config setup

---
+++ buildScripts/testSetup.js
@@ -0,0 +1,7 @@
+// This file isn't transpilled and must use CommonJS and ES8
+
+// Register babel to transpile before test runs
+require('@babel/register')();
+
+// Disable webpack features that Mocha doesn't understand
+require.extensions['.css'] = function{};
